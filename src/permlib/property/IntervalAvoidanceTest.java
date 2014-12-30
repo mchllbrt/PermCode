782e858e1b1369a4544654b953d942ca79c821b0
Michael Albert <malbert@cs.otago.ac.nz>
2014-12-31 07:54:58 +1300

    MA added a debugged interval avoidance test

--- src/permlib/property/IntervalAvoidanceTest.java
+++ src/permlib/property/IntervalAvoidanceTest.java
@@ -1,6 +1,8 @@
 package permlib.property;
 
+import java.util.Arrays;
 import permlib.Permutation;
+import permlib.Permutations;
 
 /**
  * Determines whether or not a permutation avoids another as an interval.
@@ -23,13 +25,26 @@ public class IntervalAvoidanceTest implements PermProperty {
     @Override
     public boolean isSatisfiedBy(int[] values) {
         for(int i = 0; i <= values.length - pattern.length; i++) {
-            int offset = values[i] - pattern[i];
-            if (offset < 0) continue;
+            int offset = values[i] - pattern[0];
             int j = 1;
-            while (j < pattern.length && values[j] == pattern[j] + offset) j++;
-            if (j == pattern.length) return false;
+            while (j < pattern.length && values[i+j] == pattern[j] + offset) j++;
+            if (j == pattern.length) {
+                return false;
+            }
         }
         return true;
+    }
+    
+    public static void main(String[] args) {
+        
+        Permutation p = new Permutation("132");
+        PermProperty ap = new IntervalAvoidanceTest(p);
+        for(Permutation q : new Permutations(3)) {
+            if (!ap.isSatisfiedBy(q)) System.out.println(q);
+        }
+        for(Permutation q : new Permutations(4)) {
+            if (!ap.isSatisfiedBy(q)) System.out.println(q);
+        }
     }
     
 }
